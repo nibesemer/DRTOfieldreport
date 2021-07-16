8794d41bd9ccb4e3dd160ca5f53166744687ea00
nibesemer <nib8714@gmail.com>
2021-07-16 16:12:21 -0400

    Nicole Besemer Commit 7/16/21 - 4:10pm
    combined seaFet and Current data into one file to create combined plots
    added map file to reference
    added rose plot Ian likes
    need to still work on organization and some of my plots seems like they are plotting in UTC

--- mikestrplotcode/tiltmeter-headings.R
+++ mikestrplotcode/tiltmeter-headings.R
@@ -7,27 +7,27 @@ library(magick)
 library(ggplot2)
 
 fileAny <- "~/Desktop/DRTO Data Entry/2102061_DRTO-2021-06_(0)_Current.csv"
-
+pH <- read_csv('Data/2102061_DRTO-2021-06_(0)_Current.csv',col_types = cols())
 # hardcoded limits for this DRTO file are
 # ymd_hms("2021/06/25 18:30:00") and ymd_hms("2021/06/28 19:45:00")
 # (future enhancement: incorporate Hannah Barkley's dataset trimming code;
 #  also, eliminate all the hardcoding below and calculate from data)
 
-dataAny <- read.csv(fileAny, header = TRUE)
+dataAny <- read_csv('Data/2102061_DRTO-2021-06_(0)_Current.csv',col_types = cols())
 dataAny$date = ymd_hms(paste0(dataAny$Date, dataAny$Time), tz = "UTC")
 dataAny <- dataAny[, c("date","Speed..cm.s.","Heading..degrees.")]
 colnames(dataAny) <- c("date","ws","wd")
-dataAny$dateEDT<-NA
+dataAny$DateTime<-NA
 dataAny <- subset(dataAny, date >= ymd_hms("2021/06/25 18:30:00") & date <= ymd_hms("2021/06/28 19:45:00"))
-dataAny$dateEDT <- lubridate::with_tz(dataAny$date, "America/New_York")
-dataAny
+dataAny$ <- lubridate::with_tz(dataAny$date, "America/New_York")
+
 
 # 2021/07/09 working up more tiltmeter graphs completely in R
 # (not just openair's windRose plot, which was suggested by Ian originally)
 
 # line plot (Current Speed)
 speedPlot = ggplot(data = dataAny) +
-  geom_line(aes(x = dateEDT, y = ws), col = 'dodgerblue') +
+  geom_line(aes(x = DateTime, y = ws), col = 'dodgerblue') +
   theme_bw() +
   theme(plot.margin = unit(c(5.5, 15, 5.5, 5.5), "points")) +
   scale_x_datetime(breaks = date_breaks("8 hours"),
@@ -53,6 +53,9 @@ dirPlot = ggplot(data = dataAny) +
   ylab(expression(atop("Count"))) +
   xlab(expression(atop(paste("Current Direction (", degree, ")"))))
 
+
+dirPlot
+
 #ggarrange(speedPlot, dirPlot, ncol=1, nrow=2)
 
 # save the speed and direction plots to a file
@@ -69,8 +72,11 @@ ggsave(
 roseFile=paste0(dirname(fileAny), "/tilt-plot3-rose.png")
 png(filename=roseFile,
     width = 8, height = 8, units = "in", res = 144)
-rosePlot = windRose(dataAny, ws.int=2, cols = brewer_pal(palette="Spectral", direction=-1)(6), breaks=6, paddle=FALSE, key.header="(cm/s)", key.footer="NCRMP - DRTO")
+rosePlot = windRose(dataAny, ws = "ws", wd = "wd", ws.int=2, cols = brewer_pal(palette="Spectral", direction=-1)(6), breaks=6, paddle=FALSE, key.header="(cm/s)", key.footer="NCRMP - DRTO")
 dev.off()
+
+rosePlot
+
 
 # crop off the "Frequency of counts by wind direction (%)" label
 # (which doesn't seem to be optional, in the openair package)
